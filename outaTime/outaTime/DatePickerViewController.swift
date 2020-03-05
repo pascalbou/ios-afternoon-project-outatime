88e03e697814f69777296e083a083f36c11f082a
krikaz <pascal.bou@protonmail.com>
2020-03-05 18:26:15 +0100

    storyboard

---
+++ outaTime/outaTime/DatePickerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DatePickerViewController.swift
+//  outaTime
+//
+//  Created by krikaz on 3/5/20.
+//  Copyright © 2020 thewire. All rights reserved.
+//
+
+import UIKit
+
+class DatePickerViewController: UIViewController {
+    @IBOutlet weak var MainDatePicker: UIDatePicker!
+    @IBAction func CancelButton(_ sender: UIBarButtonItem) {
+    }
+    @IBAction func DoneButton(_ sender: UIBarButtonItem) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ outaTime/outaTime/TimeCircuitsViewController.swift
@@ -0,0 +1,43 @@
+//
+//  TimeCircuitsViewController.swift
+//  outaTime
+//
+//  Created by krikaz on 3/5/20.
+//  Copyright © 2020 thewire. All rights reserved.
+//
+
+import UIKit
+
+class TimeCircuitsViewController: UIViewController {
+
+    @IBOutlet weak var DestinationTimeLabel: UILabel!
+    @IBOutlet weak var PresentTimeLabel: UILabel!
+    @IBOutlet weak var LastTimeLabel: UILabel!
+    @IBOutlet weak var SpeedLabel: UILabel!
+    
+
+    @IBOutlet weak var SetButton: UIButton!
+    @IBAction func BackButton(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
